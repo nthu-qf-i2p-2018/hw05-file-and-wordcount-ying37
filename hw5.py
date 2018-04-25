5c745313480886e872a1dbe9b61946dea256cd80
105000031 <neko182129@gmail.com>
2018-04-25 22:59:41 +0800

    hw5.py

--- hw5.py
+++ hw5.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-import ...
+import csv
 
 
 def main(filename):
     # read file into lines
-    lines = ...
+    txtfile = open(filename)
+    lines = txtfile.readlines()
 
     # declare a word list
     all_words = []
@@ -13,36 +14,39 @@ def main(filename):
     for line in lines:
         # split a line of text into a list words
         # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
+        words = line.split()
 
         # check the format of words and append it to "all_words" list
         for word in words:
             # then, remove (strip) unwanted punctuations from every word
             # "dream." => "dream"
-            word = ...
+            word = word.strip('(*&^%$#$%^&*()(*&(*&^%$#$%^&*^')
+            
             # check if word is not empty
-            if word:
-                # append the word to "all_words" list
-                all_words...
+            if word != '' :
+                 # append the word to "all_words" list
+                 all_words.append(word)
 
     # compute word count from all_words
-    counter = ...
+    from collections import Counter
+    counter = Counter(all_words)
+    counter = counter.most_common()
 
     # dump to a csv file named "wordcount.csv":
     # word,count
     # a,12345
     # I,23456
     # ...
-    with open(...) as csv_file:
+    with open('wordcount.csv','w',newline='') as csv_file:
         # create a csv writer from a file object (or descriptor)
-        writer = ...
+        writer = csv.writer(csvfile)
         # write table head
         writer.writerow(['word', 'count'])
         # write all (word, count) pair into the csv writer
-        writer.writerows(...)
+        writer.writerows(counter)
 
     # dump to a json file named "wordcount.json"
-    ...
+    json.dump(counter, open("wordcount.json", 'w'))
 
     # BONUS: dump to a pickle file named "wordcount.pkl"
     # hint: dump the Counter object directly
